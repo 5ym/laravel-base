2dc7e2fdfb2c1136a04c34bf90a88bf2f0661d63
5ym <maruyama@j-roi.com>
2019-09-05 15:53:54 +0900

    first commit

---
+++ html/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>React Sample</title>
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
+</head>
+<body>
+<div id="example"></div>
+<script src="{{asset('/js/app.js')}}"></script>
+</body>
+</html>
